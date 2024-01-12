a46011eb8ae4e393c8f85ab6ec252df8daeaaac4
Anna <147976531+AnnaKhailo@users.noreply.github.com>
2024-01-12 10:41:14 +0200

    correct task 4 (#2)

--- js/task-4.js
+++ js/task-4.js
@@ -10,17 +10,17 @@ function getShippingCost(country) {
 
         case "Chile":
         price = 250; 
-        message = `Shipping to ${country} will cost 250 credits`;
+        message = `Shipping to ${country} will cost ${price} credits`;
         break;
 
         case "Australia":
         price = 170; 
-        message = `Shipping to ${country} will cost 170 credits`;
+        message = `Shipping to ${country} will cost ${price} credits`;
         break;
 
         case "Jamaica": 
         price = 120;
-        message = `Shipping to ${country} will cost 120 credits`;
+        message = `Shipping to ${country} will cost ${price} credits`;
         break;
         
         default:
